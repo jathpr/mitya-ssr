4b208327b0a852b5842cbe26daf8dfcacef1e706
Dzmitry Martyniuk <jathpr@gmail.com>
2024-06-18 22:43:05 +0400

    first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Kuku
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Image width={600} height={600} src="/face.jpg" alt="My face" />
+        <span className="relative top-4 left-1/3">Kuku</span>
+      </main>
+    </>
   );
 }
+
+// position: relative;
+// /* right: 10px; */
+// top: 20px;
+// left: 30vw;
